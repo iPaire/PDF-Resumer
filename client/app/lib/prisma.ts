8cf176cb300199780099093f88ed9f829c8e2e76
iPaire <pairegames@gmail.com>
2025-06-20 11:13:14 +0300

    functioneaza build! (cu ignore types si unused)

--- client/app/lib/prisma.ts
+++ client/app/lib/prisma.ts
@@ -1,13 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined
 }
 
-const prisma = globalThis.prisma || new PrismaClient();
+const prisma = global.prisma || new PrismaClient({
+  log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error']
+})
 
-if (process.env.NODE_ENV !== 'production') {
-  globalThis.prisma = prisma;
-}
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
 
-export default prisma;
+export default prisma
